33880005c4b56630456f8036285659b5cb0ecfa2
Evgenii <evgeniifedorovskii95@mail.ru>
2024-05-15 11:08:00 +0700

    Изменения на страницах формы

--- src/js/upload.js
+++ src/js/upload.js
@@ -8,22 +8,26 @@ $(document).ready(function () {
     });
 
     // Делегируем событие 'change' на документ для элементов с классом 'inputfile'
-    $(document).on('change', '.inputfile', function (e) {
-        var input = $(this);
-        var label = input.next('label');
-        var labelVal = label.html();
-        var fileName = '';
+	$(document).on('change', '.inputfile', function (e) {
+		var input = $(this);
+		var label = input.next('label');
 
-        if (this.files && this.files.length > 1)
-            fileName = (this.getAttribute('data-multiple-caption') || '').replace('{count}', this.files.length);
-        else
-            fileName = e.target.value.split('\\').pop();
-
-        if (fileName)
-            label.find('.upload__caption').html(fileName);
-        else
-            label.html(labelVal);
-    });
+		var name_file = [];
+		let sum = 0;
+		let ending = '';
+		for (var i = 0; i < input.get(0).files.length; ++i) {
+			name_file.push(input.get(0).files[i].name);
+			sum++;
+		} 
+		if (sum === 1) {
+			ending = '';
+		} else if (sum >= 2 && sum <= 4) {
+			ending = 'а';
+		} else if (sum >= 5) {
+			ending = 'ов';
+		}
+		label.find('.upload__caption').html(`Выбрано ${sum} файл${ending}`);
+	});
 
 	// Обработчик клика для кнопки добавления нового блока 'upload-file'
 	$(document).on('click', '.upload__btn', function () {
@@ -43,9 +47,12 @@ $(document).ready(function () {
 		clone.find('input[type="text"]').val('');
 		clone.find('input[type="file"]').val('');
 		clone.find('.upload__caption').html('Загрузите изображение <span>(JPG, JPEG, PNG. Размер до 2 Мб)</span>');
-		var newId = 'file-' + ($('.upload-file').length + 1);
-		clone.find('input[type="file"]').attr('id', newId);
+		var fileCount = briefFormLocation.find('.upload-file').length + 1;
+		var newId = 'file-' + fileCount;
+		var newName = 'location-file-' + fileCount + '[]';
+		clone.find('input[type="file"]').attr('id', newId).attr('name', newName);
 		clone.find('.upload__box').attr('for', newId);
+		
 		if (clone.find('.upload__deleate').length === 0) {
 			clone.append('<button class="upload__deleate">-</button>');
 		}
